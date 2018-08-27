e9e2a36c82ddd49a8b42f0de02d5de49e058f52e
Collin Tod <squablesquable@gmail.com>
2018-08-27 12:29:03 -0400

    Convert pointer based linked-list into + based.
    Since the potential exists for the entire heap to move when a memory
    block is freed, the pointers in mem_block_meta are not guarenteed to
    point to the next mem_block_meta. However, since the meta blocks contain
    the size of the succeeding memory block, simple pointer arithmetic can
    be used to find the next meta block, in a wholly relative way.
    Therefore, this new memory traversal method is agnostic to the absolute
    position of the heap as a whole, making it far more flexible.
    
    However, this method does sacrifice a little speed since arithmetic and
    sizeof evalutations need to be done each time instead of a simple
    dereference.

--- allpar.c
+++ allpar.c
@@ -1,3 +1,4 @@
+//TODO: convert all comments that span multiple lines into block comments
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,32 +8,56 @@ typedef unsigned char byte;
 struct mem_block_meta{
 	int size;
 	int MUID;
-	struct mem_block_meta* next;
+	//struct mem_block_meta* next;
 };
 
 
 static struct mem_block_meta* global_top;
+static struct mem_block_meta* global_bot;
 
+static int top_MUID = 1;
 
-static int top_MUID = 0;
+/**
+ * This function places a mem_block_meta of all 0s on top of the heap, to act as
+ * a NUL terminator for the heap's memory during traversal.
+ *
+ * @return: a pointer to this new mem_block_meta
+ */
+struct mem_block_meta* push_zero_block(){
+	struct mem_block_meta* zero_block = sbrk(0);
+	sbrk(sizeof(struct mem_block_meta));
+	zero_block->size = 0;
+	zero_block->MUID = 0;
+	return zero_block;
+}
 
 struct mem_block_meta *request_mem_block(int size){
 
+	// Creates the very first 0 block in order to act as a base. Is only run the
+	// very first allocation. 
+	if(global_top == NULL){
+		global_top = push_zero_block();
+		global_bot = global_top;
+	}
 
+	// Moves the program break back to erase the current zero block
+	sbrk(-sizeof(struct mem_block_meta));
+
+	// Places the new mem_block where the old zero block used to be
 	struct mem_block_meta* meta = sbrk(0);
 	sbrk(size + sizeof(struct mem_block_meta));
 	
 	//TODO: guarentee that sbrk succeeds 
 
-	meta->next = global_top;
-
+	// Sets everything about the new mem_block
 	global_top = meta;
-
 	meta->size = size;
-
 	meta->MUID = top_MUID;
 	top_MUID++;
 	
+	// Pushes a new zero block to act as NUL terminator
+	global_top = push_zero_block();
+
 	return meta;
 }
 	
@@ -45,11 +70,19 @@ int mallocp(int size){
 }
 
 struct mem_block_meta *retrieve_memory_meta_block(int MUID){
-	struct mem_block_meta* current = global_top;
-	while(current && current->MUID != MUID){
-		current = current->next;
+	struct mem_block_meta* current = global_bot;
+	while(current != global_top && current->MUID != MUID){
+
+		//Sets the current ptr to look at the next mem_block_meta. This will not
+		//overflow into unallocated memory as the while loop makes sure that
+		//current is not the global_top.
+		current = (struct mem_block_meta*)((char*)current + current->size + \
+				sizeof(struct mem_block_meta));
 	}
-	return current;
+	
+	// Returns NULL if traversing the heap did not find the MUID asked for, i.e.
+	// current points to the NUL terminator of the memory, i.e. global_top 
+	return current == global_top ? NULL : current;
 }
 
 void *retrieve_memory(int MUID){
@@ -57,16 +90,15 @@ void *retrieve_memory(int MUID){
 }
 
 
-
 int main(void){
 	int number1ID = mallocp(sizeof(int));
-	*(int*)retrieve_memory(number1ID) = 5;
+	*(int*)retrieve_memory(number1ID) = 1;
 
 	int number2ID = mallocp(sizeof(int));
 	*(int*)retrieve_memory(number2ID) = 10000;
 
-	int stringID = mallocp(sizeof("TEST"));
-	strncpy((char*)retrieve_memory(stringID), "TEST", 5);
+	int stringID = mallocp(sizeof("HELLO WORLD"));
+	strncpy((char*)retrieve_memory(stringID), "HELLO WORLD",255);
 
 	printf("%d\n", *(int*)retrieve_memory(number1ID));
 	printf("%s\n", (char*)retrieve_memory(stringID));
